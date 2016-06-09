926037b0a2a73a9faed687f7f4224747df4daf33
Jordan Rhea <rheajt@gmail.com>
2016-06-09 20:05:42 +0300

    second iteration, added jquery

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,17 @@
 $(function() {
-  $('.tldr').append('<h3>Too long; Didn\'t read</h3>');
+
+  $.getJSON('https://script.google.com/macros/s/AKfycbzv1Yp4YpCA5cHH1HO7SMUFm_lppDhOntX74gTS-eM52kJ8rTHP/exec?callback=?', function(json) {
+    var html = '<h3>Innovator Corkboard</h3>';
+    html += '<ol>';
+
+    json.forEach(function(each) {
+      html += '<li>' + each + '</li>';
+    });
+
+    html += '</ol>';
+
+    $('.tldr').append(html);
+
+  });
+
 });
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Innovator dashboard
+
+### Trying to condense the many forms of communication of the #COL16 crew for the less
+### social media aware members of the group.
+
+Also, trying to learn a little bit about Chrome extensions in the process
+
+Run bower install to get started.
