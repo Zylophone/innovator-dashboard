49ab1866132bc3af6997ab7ca7e6d5b357dcd9a1
Jordan Timothy RHEA <rheajt@gmail.com>
2016-06-13 14:48:59 +0300

    reversed the return array

--- dashboard.js
+++ dashboard.js
@@ -2,9 +2,9 @@ $(function() {
 
   $.getJSON('https://script.google.com/macros/s/AKfycbzv1Yp4YpCA5cHH1HO7SMUFm_lppDhOntX74gTS-eM52kJ8rTHP/exec?callback=?', function(json) {
     var html = '<h3>Innovator Corkboard</h3>';
-    html += '<ol>';
+    html += '<ol reversed>';
 
-    json.forEach(function(each) {
+    json.reverse().forEach(function(each) {
       html += '<li>' + each + '</li>';
     });
 
