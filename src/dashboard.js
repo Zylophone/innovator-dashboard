f0426f844def69b3ce460ed86e3f82ae312dbe5e
Jordan Timothy RHEA <rheajt@gmail.com>
2016-10-14 12:54:16 +0300

    in progress

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,28 +1,51 @@
-function Ajax() {
-  return {
-    get: function() {
-      var request = new XMLHttpRequest();
-      request.open('GET', '/my/url', true);
+// function Ajax() {
+//   return {
+//     get: function() {
+//       var request = new XMLHttpRequest();
+//       request.open('GET', '/my/url', true);
+//
+//       request.onload = function() {
+//         if (request.status >= 200 && request.status < 400) {
+//           // Success!
+//           return request.responseText;
+//         } else {
+//           // We reached our target server, but it returned an error
+//           return request.error;
+//         }
+//       };
+//     },
+//     post: function() {
+//       var request = new XMLHttpRequest();
+//       request.open('POST', '/my/url', true);
+//       request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//
+//       // Here is an example of a request header with access_token inside;
+//       // xhr.setRequestHeader('Authorization', 'Bearer ' + oauthToken.access_token);
+//       //
+//       request.send(data);
+//     }
+//   };
+// }
+function getTasks() {
+  chrome.identity.getAuthToken({interactive: true}, function(token) {
+    var url = 'https://www.googleapis.com/tasks/v1/users/@me/lists';
+    var request = new XMLHttpRequest();
 
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-          // Success!
-          return request.responseText;
-        } else {
-          // We reached our target server, but it returned an error
-          return request.error;
-        }
-      };
-    },
-    post: function() {
-      var request = new XMLHttpRequest();
-      request.open('POST', '/my/url', true);
-      request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.open('GET', url, true);
+    request.setRequestHeader('Authorization', 'Bearer ' + token);
 
-      // Here is an example of a request header with access_token inside;
-      // xhr.setRequestHeader('Authorization', 'Bearer ' + oauthToken.access_token);
-      // 
-      request.send(data);
-    }
-  };
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        console.log(JSON.parse(request.responseText));
+      } else {
+        // We reached our target server, but it returned an error
+        console.log(JSON.parse(request.error));
+      }
+    };
+
+    request.send();
+  });
 }
+
+document.getElementById('get-tasks').addEventListener('click', getTasks);
